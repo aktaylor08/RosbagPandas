63cdf3c49376610688bcf7a898160b9d69e1fc50
Low Kian Seong <kianseong@gmail.com>
2014-06-18 14:18:42 +0800

    Update setup.py
    Add a line of description so that project don't show `UNKNOWN` for pip searches and pypi listings.

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from distutils.core import setup
 setup(name='rosbag_pandas',
         version='0.3.1',
+        description='Create a Python pandas data frame from a ros bag file',
         py_modules=['rosbag_pandas'],
         scripts=['scripts/bag_graph.py', 'scripts/bag2csv.py'],
      )
