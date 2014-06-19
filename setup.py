498be788eaff208b337560b2081934aa9cfd9669
Adam Taylor <aktaylor08@gmail.com>
2014-06-19 09:35:49 -0500

    updates to this forgot to merge

--- setup.py
+++ setup.py
@@ -1,13 +1,6 @@
 from distutils.core import setup
 setup(name='rosbag_pandas',
-<<<<<<< HEAD
-        version='0.3.3',
+        version='0.3.4',
         description='Create a Python pandas data frame from a ros bag file',
         py_modules=['rosbag_pandas'],
-        scripts=['scripts/bag_graph.py', 'scripts/bag2csv.py'],
-     )
-=======
-      version='0.3.1',
-      py_modules=['rosbag_pandas'],
-      scripts=['scripts/bag_graph.py', 'scripts/bag2csv.py'], )
->>>>>>> e38797b322d27ce51b9d3a238bfca66759b98892
+        scripts=['scripts/bag_graph.py', 'scripts/bag2csv.py'],)
